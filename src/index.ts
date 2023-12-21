7ca66c144b9c51b6dbb70483a81794a1786fee51
Trond Albinussen <trond.albinussen@gmail.com>
2023-12-21 09:45:27 +0100

    Ejs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
+import EJSLayout from 'express-ejs-layouts';
+
 import env from './env';
 
-
-console.log(env.PORT);
-
-
-console.log(env.PORT);
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(EJSLayout);
+
 
 app.get('/', (req, res) => {
-    res.send('bobbo3');
+    res.render('home');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.get('/protected', (req, res) => {
+    res.render('protected');
 })
 
 app.listen(3000, () => {
-    console.log(`App up and running on port: ${process.env.PORT}`)
+    console.log(`App up and running on port: ${env.PORT}`)
 });
